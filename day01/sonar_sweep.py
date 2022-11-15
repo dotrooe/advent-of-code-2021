9103e4475cb3bd4abb907c51c8099b317fa8e87b
hubert <hubertszymanski5@gmail.com>
2022-11-15 18:33:34 +0100

    day-01: Part II - solved

--- day01/sonar_sweep.py
+++ day01/sonar_sweep.py
@@ -3,7 +3,8 @@ from utils.file_utils import read_num_list
 
 def main():
     arr = read_num_list('input.txt')
-    print(measure_increases(arr))
+    print(f'part I: {measure_increases(arr)}')
+    print(f'part II: {measure_increases_of_triplets(arr)}')
 
 
 def measure_increases(arr: list):
@@ -12,6 +13,14 @@ def measure_increases(arr: list):
         if arr[i] > arr[i - 1]:
             result += 1
     return result
+
+
+def measure_increases_of_triplets(arr: list):
+    triplets = []
+    for i in range(len(arr) - 2):
+        suma = arr[i] + arr[i + 1] + arr[i + 2]
+        triplets.append(suma)
+    return measure_increases(triplets)
 
 
 if __name__ == '__main__':
--- day01/test/sonar_sweep_test.py
+++ day01/test/sonar_sweep_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day01.sonar_sweep import measure_increases
+from day01.sonar_sweep import measure_increases, measure_increases_of_triplets
 from utils.file_utils import read_num_list
 
 
@@ -9,6 +9,10 @@ class SonarSweepTestCase(unittest.TestCase):
     def test_should_pass_advent_of_code_part_one_example(self):
         arr = read_num_list("example-input.txt")
         self.assertEqual(7, measure_increases(arr))
+
+    def test_should_pass_advent_of_code_part_two_example(self):
+        arr = read_num_list("example-input.txt")
+        self.assertEqual(5, measure_increases_of_triplets(arr))
 
 
 if __name__ == '__main__':
